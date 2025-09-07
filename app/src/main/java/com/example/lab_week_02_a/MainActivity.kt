06a9c8410bc2309358655549ac1d9f98117bbc28
Muiz-Isman <muizisman511@gmail.com>
2025-09-07 12:58:42 +0700

    Commit No. 2 – Result activity [Intent]

--- app/src/main/java/com/example/lab_week_02_a/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_02_a/MainActivity.kt
@@ -1,16 +1,53 @@
 package com.example.lab_week_02_a
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.lab_week_02_a.R
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val COLOR_KEY = "COLOR_KEY"
+    }
+
+    private val submitButton: Button
+        get() = findViewById(R.id.submit_button)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        submitButton.setOnClickListener {
+            val colorCode = findViewById<TextInputEditText>(R.id.color_input_field).text.toString()
+
+            if (colorCode.isNotEmpty()) {
+                if (colorCode.length < 6) {
+                    Toast.makeText(
+                        this,
+                        getString(R.string.color_code_input_wrong_length),
+                        Toast.LENGTH_LONG
+                    ).show() // Perbaikan 1
+                } else {
+                    val resultIntent = Intent(this, ResultActivity::class.java) // Perbaikan 2 & 3
+                    resultIntent.putExtra(COLOR_KEY, colorCode) // Perbaikan 4
+                    startActivity(resultIntent) // Perbaikan 5
+                }
+            } else {
+                Toast.makeText(
+                    this,
+                    getString(R.string.color_code_input_empty),
+                    Toast.LENGTH_LONG
+                ).show() // Perbaikan 1
+            }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
